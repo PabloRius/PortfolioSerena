bbabfcc3a1294b3d9971abae1bee061656fe197c
PabloRius <pablogrius@gmail.com>
2025-02-12 10:51:07 +0000

    Change video loading optimisation for load more button

--- src/pages/CC.tsx
+++ src/pages/CC.tsx
@@ -2,23 +2,18 @@ import { useState } from 'react';
 import { InstagramEmbed } from 'react-social-media-embed';
 import { DefaultLayout } from '../layouts/DefaultLayout';
 import { videos } from '../data/videos';
-import { useInView } from 'react-intersection-observer';
 import '../styles/CCPage.css';
 
 export const CC = () => {
-  // Número de videos iniciales a mostrar
-  const INITIAL_COUNT = 6;
-  const LOAD_MORE_COUNT = 6; // Cuántos videos cargar en cada paso
+  const INITIAL_COUNT = 6; // Número de videos a mostrar inicialmente
+  const LOAD_MORE_COUNT = 6; // Cuántos videos cargar con cada clic
 
   const [loadedVideos, setLoadedVideos] = useState(INITIAL_COUNT);
-  const { ref, inView } = useInView({
-    threshold: 0.5, // Dispara cuando el 50% del elemento es visible
-  });
 
-  // Cuando el usuario hace scroll y el último video visible entra en pantalla, cargamos más
-  if (inView && loadedVideos < videos.length) {
+  // Función para cargar más videos
+  const handleLoadMore = () => {
     setLoadedVideos((prev) => Math.min(prev + LOAD_MORE_COUNT, videos.length));
-  }
+  };
 
   return (
     <DefaultLayout>
@@ -26,9 +21,8 @@ export const CC = () => {
         <h2
           style={{
             margin: '30px auto',
-            maxWidth: '90%',
+            width: 'max-content',
             fontSize: '32px',
-            textAlign: 'center',
           }}
         >
           Click on any video to watch it!
@@ -43,12 +37,9 @@ export const CC = () => {
             ))}
         </ul>
         {loadedVideos < videos.length && (
-          <div
-            ref={ref}
-            style={{ height: '10px', margin: '20px auto', textAlign: 'center' }}
-          >
-            Loading more...
-          </div>
+          <button className="LoadMoreButton" onClick={handleLoadMore}>
+            Load More Videos
+          </button>
         )}
       </div>
     </DefaultLayout>
